e07ede17c1fd6120789c29727e80ad3294cd2aeb
Harison Gonçalves de Souza <harisong.souza@gmail.com>
2022-05-19 21:45:26 -0300

    content of pages created

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 function openPageBtnAjudenos() {
     window.location.href = "page7.html";
 }
+
+function submitForm() {
+    
+    var name = document.getElementById("inputName").value;
+    var sobrenome = document.getElementById("inputSobrenome").value;
+    var mensagem = document.getElementById("inputMensagem").value;
+    
+    var link = "mailto:FakeEmail@gmail.com"
+            + "?cc=FakeEmail@gmail.com"
+            + "&subject=" + name + sobrenome
+            + "&body=" + mensagem;
+    
+    window.alert("Dados confirmados com sucesso!\nClique em enviar email.")
+
+    document.getElementById("labelEnviaEmail").innerHTML = "Enviar Email".link(link);
+    
+}
